1dea0a1bd21db075b8bfb5b87ba208377a94e33e
Abogeerick <abogeerick@gmail.com>
2023-06-23 05:26:10 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const text=document.querySelector("p");
+function ContentLoaded(){
+  text.textContent="This is really cool!"
+}
+document.addEventListener("DOMContentLoaded", ContentLoaded);
